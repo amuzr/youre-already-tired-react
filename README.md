3bf0a84e8882b6e1437b7a5b3e6153572781ae1d
WangSeonho <wang@nhn.com>
2017-06-20 13:54:23 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## 너는 이미 피곤하다
-
-- React, Redux, Material UI with create-react-app
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import main from "./main";
-import charts from "./charts";
-
-export default combineReducers({
-  main,
-  charts
-});
